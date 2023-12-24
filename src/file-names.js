a0bb56695f15fae3c09d183b394840b8f6b5ee2f
Anton <137609245+Ddonec@users.noreply.github.com>
2023-12-24 15:44:45 +0300

    feat: conplet string repear and file name tasks

--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,11 +15,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+   const uniqName = new Set();
+   const count = new Map();
+
+   return names.map((name) => {
+      if (!uniqName.has(name)) {
+         uniqName.add(name);
+         return name;
+      } else {
+         const elseCount = count.has(name) ? count.get(name) : 1;
+         const newName = `${name}(${elseCount})`;
+         count.set(name, elseCount + 1);
+         uniqName.add(newName);
+         return newName;
+      }
+   });
 }
 
 module.exports = {
-  renameFiles
+   renameFiles,
 };
